0861aaf436294d0571fac2eee5d9e914cd4650dd
Yun Lu <luyun198993@gmail.com>
2021-04-07 13:48:49 -0700

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -d --init --network=host --restart=always --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/dbus:/var/run/dbus 587413328198.dkr.ecr.us-west-1.amazonaws.com/codeserver:latest
+docker run -d --name=code-server --init --network=host --restart=always --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/dbus:/var/run/dbus 587413328198.dkr.ecr.us-west-1.amazonaws.com/codeserver:latest

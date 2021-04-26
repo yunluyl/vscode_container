012c780d58a55d9896f871d778c520619910c8be
Yun Lu <luyun198993@gmail.com>
2021-04-26 16:11:00 -0700

    Update aws_user_data.sh

--- aws_user_data.sh
+++ aws_user_data.sh
@@ -6,4 +6,5 @@ usermod -a -G docker ec2-user
 chkconfig docker on
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 587413328198.dkr.ecr.us-west-1.amazonaws.com
 docker pull 587413328198.dkr.ecr.us-west-1.amazonaws.com/codeserver:latest
-docker run -d --network=host --restart=always 587413328198.dkr.ecr.us-west-1.amazonaws.com/codeserver:latest
+docker run -d --name=code-server --init --network=host --restart=always --privileged -v /var/run/docker.sock:/var/run/docker.sock 587413328198.dkr.ecr.us-west-1.amazonaws.com/codeserver:latest
+
